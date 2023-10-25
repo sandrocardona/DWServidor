6c74f030d65a8e07c733c34a25cdb902e21cd755
sandrocardona <sandrocardona41@gmail.com>
2023-10-25 20:41:53 +0200

    25 OCT 20.42

--- practica_examen/ejercicio3.php
+++ practica_examen/ejercicio3.php
@@ -1,52 +1,98 @@
 <?php
-/*Control de errores*/
-if(isset($_POST["btnEnviar"])){
+function mi_strlen($texto)
+{
+    $cont=0;
+    while(isset($texto[$cont]))
+        $cont++;
+
+    return $cont;
+}
+
+function mi_explode($sep,$texto)
+{
+    $aux=[];
+    $l_texto=mi_strlen($texto);
+    $i=0;
+    while($i<$l_texto && $texto[$i]==$sep)
+        $i++;
+
+
+    if($i<$l_texto)
+    {
+        $j=0;
+        $aux[$j]=$texto[$i];
+        for($i=$i+1;$i<$l_texto;$i++)
+        {
+            if($texto[$i]!=$sep)
+            {
+                $aux[$j].=$texto[$i];
+            }
+            else
+            {
+
+                while($i<$l_texto && $texto[$i]==$sep)
+                    $i++;
+                
+                if($i<$l_texto)
+                {
+                    $j++;
+                    $aux[$j]=$texto[$i];
+                }
+                
+            }
+        }
+
+
+    }
+    
+    return $aux;
+}
+
+
+if(isset($_POST["btnContar"]))
+{
     $error_form=$_POST["texto"]=="";
 }
-/*Funciones*/
-function my_explode($texto){
-    $i=0;
-    $arr=[];
-    while($texto[$i]!=$_POST["opt"]){
-        
-    }
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 3</title>
+    <title>Ejercicio3 Exam Anterior</title>
     <style>
-        .error{color: red}
+        .error{color:red}
     </style>
 </head>
 <body>
-    <h1>Ejericio 3</h1>
-    <form action="ejercicio3.php" method="post" enctype="multipart/form-data">
+    <h1>Ejercicio 3</h1>
+    <form action="ejercicio3.php" method="post">
         <p>
-            <label for="texto">Introduce un texto</label>
-            <input type="text" name="texto" id="texto">
-            <select name="opt" id="opt">
-                <option value=",">coma</option>
-                <option value=".">punto</option>
-                <option value=";">punto y coma</option>
-                <option value=" ">espacio</option>
-                <option value=":">dos puntos</option>
+            <label for="sep">Elija Separador: </label>
+            <select name="sep" id="sep">
+                <option <?php if(isset($_POST["btnContar"]) && $_POST["sep"]==",") echo "selected";?> value=",">, (coma)</option>
+                <option <?php if(isset($_POST["btnContar"]) && $_POST["sep"]==";") echo "selected";?> value=";">; (punto y coma)</option>
+                <option <?php if(isset($_POST["btnContar"]) && $_POST["sep"]==" ") echo "selected";?> value=" "> (espacio)</option>
+                <option <?php if(isset($_POST["btnContar"]) && $_POST["sep"]==":") echo "selected";?> value=":">: (dos puntos)</option>
             </select>
-            <button type="submit" name="btnEnviar" id="btnEnviar">Enviar</button>
+        </p>
+        <p>
+            <label for="texto">Introduzca una frase: </label>
+            <input type="text" name="texto" id="texto" value="<?php if(isset($_POST["texto"])) echo $_POST["texto"];?>">
             <?php
-            if(isset($_POST["btnEnviar"]) && $error_form){
-                echo "<p class='error'>Campo vacío</p>";
-            }
+            if(isset($_POST["btnContar"]) && $error_form)
+                echo "<span class='error'>Campo vacío</span>";
             ?>
+        </p>
+        <p>
+            <button type="submit" name="btnContar">Contar</button>
         </p>
     </form>
     <?php
-    if(isset($_POST["btnEnviar"]) && !$error_form){
-        echo "<p>funciona</p>";
+    if(isset($_POST["btnContar"]) && !$error_form)
+    {
+        echo "<h2>Respuesta</h2>";
+        echo "<p>El número de palabras del texto introducido separadas por el separador seleccionado es de :".count(mi_explode($_POST["sep"],$_POST["texto"]))."</p>";
     }
     ?>
 </body>
