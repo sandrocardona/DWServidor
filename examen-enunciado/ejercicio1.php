8957ff79f43680e9e3f1ed3fb6d08d22facf8410
sandrocardona <sandrocardona41@gmail.com>
2023-10-30 08:29:43 +0100

    30 OCT

--- examen-enunciado/ejercicio1.php
+++ examen-enunciado/ejercicio1.php
@@ -21,6 +21,85 @@ function first(){
     if(isset($_POST["btnEnviar"])){
         echo "<p>Pulsado</p>";
     }
+
+
+    function mi_explode($sep,$texto)
+{
+    $aux=[];
+    $l_texto=mi_strlen($texto);
+    $i=0;
+    while($i<$l_texto && $texto[$i]==$sep)
+        $i++;
+
+
+    if($i<$l_texto)
+    {
+        $j=0;
+        $aux[$j]=$texto[$i];
+        for($i=$i+1;$i<$l_texto;$i++)
+        {
+            if($texto[$i]!=$sep)
+            {
+                $aux[$j].=$texto[$i];
+            }
+            else
+            {
+
+                while($i<$l_texto && $texto[$i]==$sep)
+                    $i++;
+                
+                if($i<$l_texto)
+                {
+                    $j++;
+                    $aux[$j]=$texto[$i];
+                }
+                
+            }
+        }
+
+
+    }
+    
+    return $aux;
+}function mi_explode($sep,$texto)
+{
+    $aux=[];
+    $l_texto=mi_strlen($texto);
+    $i=0;
+    while($i<$l_texto && $texto[$i]==$sep)
+        $i++;
+
+
+    if($i<$l_texto)
+    {
+        $j=0;
+        $aux[$j]=$texto[$i];
+        for($i=$i+1;$i<$l_texto;$i++)
+        {
+            if($texto[$i]!=$sep)
+            {
+                $aux[$j].=$texto[$i];
+            }
+            else
+            {
+
+                while($i<$l_texto && $texto[$i]==$sep)
+                    $i++;
+                
+                if($i<$l_texto)
+                {
+                    $j++;
+                    $aux[$j]=$texto[$i];
+                }
+                
+            }
+        }
+
+
+    }
+    
+    return $aux;
+}
     ?>
 </body>
 </html>
