71a41ad84c4615df4df34a8383bcc2b1b155badf
sandrocardona <sandrocardona41@gmail.com>
2023-11-15 13:49:53 +0100

    15 NOV

--- EJ_BD/PRACT_8/index.php
+++ EJ_BD/PRACT_8/index.php
@@ -1,3 +1,7 @@
+<?php
+require "src/cts_functions.php";
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
---
+++ EJ_BD/PRACT_8/src/cts_functions.php
@@ -0,0 +1,11 @@
+<?php
+define("SERVIDOR_BD","localhost");
+define("USUARIO_BD","jose");
+define("CLAVE_BD","josefa");
+define("NOMBRE_BD","bd_foro");
+
+/* define("SERVIDOR_BD","localhost");
+define("USUARIO_BD","root");
+define("CLAVE_BD","");
+define("NOMBRE_BD","clientes_bd"); */
+?>
