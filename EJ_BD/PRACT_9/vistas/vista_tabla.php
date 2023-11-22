f9630aea8be9c2f039fdcd76d5e2cecb9f28bb5f
sandrocardona <sandrocardona41@gmail.com>
2023-11-22 14:34:21 +0100

    22 nov

--- EJ_BD/PRACT_9/vistas/vista_tabla.php
+++ EJ_BD/PRACT_9/vistas/vista_tabla.php
@@ -1,33 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        table{
-            border: 1px solid black;
-            width: 75%;
-        }
-        th{
-            border: 1px solid black;
-            background-color:grey;
-        }
+        table,td,th{border:1px solid black;}
+        table{border-collapse:collapse;text-align:center;width:90%;margin: 0 auto;}
+        th{background-color:lightgrey;}
+        .enlace{border:none;background:none;color:blue;text-decoration:underline;}
+        h1,h2{text-align:center;}
     </style>
 </head>
 <body>
+    <h1>VideoClub</h1>
+    <h2>Listado de películas</h2>
+    <?php
+        /* Si no hay conexión, me conecto a la base de datos */
+        if(!isset($conexion)){
+            try{
+                $conexion=mysqli_connect(DIRECCION,USUARIO,PASSWORD,BASE_DATOS);
+                mysqli_set_charset($conexion, "utf8");
+            }
+            catch(Excepcion $e){
+                echo "<p>No se ha podido conectar a la BD: ".$e->getMessage()."</p>";
+            }
+        }
+        /* Realizo la consulta */
+        try{
+            $consulta = "select * from peliculas";
+            $resultado=mysqli_query($conexion, $consulta);
+        }
+        catch(Excepcion $e){
+            echo "<p>No se ha podido realizar la consulta: ".$e -> getMessage()."</p>";
+        }
+    ?>
     <table>
         <tr>
             <th>Id</th>
             <th>Título</th>
             <th>Carátula</th>
-            <th><button>Películas +</button></th>
+            <th><button class='enlace'>Películas +</button></th>
         </tr>
         <?php
-        while(){
-            
+        while($tupla=mysqli_fetch_assoc($resultado)){
+            echo "<tr>";
+            echo "<td>".$tupla["idPelicula"]."</td>";
+            echo "<td>".$tupla["titulo"]."</td>";
+            echo "<td>".$tupla["caratula"]."</td>";
+            echo "<td><form action='index.php' method='post'><button class='enlace' type='submit' value='".$tupla["idPelicula"]."'>Borrar</button> - <button class='enlace' type='submit' value='".$tupla["idPelicula"]."'>Editar</button></form></td>";
+            echo "</tr>";
         }
         ?>
     </table>
+    <?php
+    mysqli_free_result($resultado);
+    mysqli_close($conexion);
+    ?>
 </body>
 </html>
