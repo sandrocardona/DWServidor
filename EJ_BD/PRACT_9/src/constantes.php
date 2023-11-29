4d19f06ce4ced1af15d8e0321b48434017047f45
sandrocardona <sandrocardona41@gmail.com>
2023-11-29 17:23:30 +0100

    29 nov casa

--- EJ_BD/PRACT_9/src/constantes.php
+++ EJ_BD/PRACT_9/src/constantes.php
@@ -1,6 +1,11 @@
 <?php
-define("DIRECCION","localhost");
+/* define("DIRECCION","localhost");
 define("USUARIO","jose");
 define("PASSWORD","josefa");
+define("BASE_DATOS","bd_videoclub"); */
+
+define("DIRECCION","localhost");
+define("USUARIO","root");
+define("PASSWORD","");
 define("BASE_DATOS","bd_videoclub");
 ?>
