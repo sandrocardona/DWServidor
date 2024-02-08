d30f886a47c229fad249fa3ae85b7f94905398c5
sandrocardona <sandrocardona41@gmail.com>
2024-02-08 19:05:52 +0100

    8 febrero casa

--- TEMA6/Examen_SW_22_23_2/servicios_rest/index.php
+++ TEMA6/Examen_SW_22_23_2/servicios_rest/index.php
@@ -12,11 +12,6 @@ $app->get('/conexion_PDO',function($request){
     echo json_encode(conexion_pdo());
 });
 
-$app->get('/conexion_MYSQLI',function($request){
-    
-    echo json_encode(conexion_mysqli());
-});
-
 
 
 // Una vez creado servicios los pongo a disposición
