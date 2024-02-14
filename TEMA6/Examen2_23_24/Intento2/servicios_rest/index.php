9ca1d851abd96f3d37fbf6e6cbaacc93ef0abc88
sandrocardona <sandrocardona41@gmail.com>
2024-02-14 17:42:34 +0100

    14 febrero casa

--- TEMA6/Examen2_23_24/Intento2/servicios_rest/index.php
+++ TEMA6/Examen2_23_24/Intento2/servicios_rest/index.php
@@ -5,11 +5,12 @@ require __DIR__ . '/Slim/autoload.php';
 
 $app= new \Slim\App;
 
-
-
 $app->get('/conexion_PDO',function($request){
     echo json_encode(conexion_pdo());
 });
+
+
+
 // Una vez creado servicios los pongo a disposición
 $app->run();
 ?>
